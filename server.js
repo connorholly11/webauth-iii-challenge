237fdc19ac912081a2054a9f4aee1cbe8423fbf7
Connor Holly <connorholly11@gmail.com>
2019-10-16 13:42:56 -0400

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const usersRouter = require("./data/users/users.router");
+const cors = require("cors");
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use("/api", usersRouter);
 
